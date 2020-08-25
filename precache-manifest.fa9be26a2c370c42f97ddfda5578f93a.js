23796ff66b775e7b2fbe7478d0fdf166f5f413f0
Emerson <captainscheffer@gmail.com>
2020-08-24 17:13:32 -0700

    Updates

---
+++ precache-manifest.fa9be26a2c370c42f97ddfda5578f93a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad467f1eb417179d1a36cff4b2ab227",
+    "url": "/emersondev/index.html"
+  },
+  {
+    "revision": "4e071eaa38fc467c16b6",
+    "url": "/emersondev/static/css/main.a1acbf48.chunk.css"
+  },
+  {
+    "revision": "7cad453718e13123c28d",
+    "url": "/emersondev/static/js/2.ba932ea0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/emersondev/static/js/2.ba932ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e071eaa38fc467c16b6",
+    "url": "/emersondev/static/js/main.c1203d6d.chunk.js"
+  },
+  {
+    "revision": "5de8419fe1a0b825f741",
+    "url": "/emersondev/static/js/runtime-main.e9ecbcd1.js"
+  },
+  {
+    "revision": "da3af27495f32a81a4e26b1d01c54195",
+    "url": "/emersondev/static/media/naAmericaLogo.da3af274.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/emersondev/precache-manifest.38a479051c7e44ef9ed7118e1d6a1d69.js"
+  "/emersondev/precache-manifest.fa9be26a2c370c42f97ddfda5578f93a.js"
 );
 
 self.addEventListener('message', (event) => {
